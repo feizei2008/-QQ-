1d42f5a8c9eabe042b917f21ebe7a7c853cfb364
feizei2008 <zacklike@hotmail.com>
2018-08-23 19:26:14 +0800

    Update xlsx2bmp.py

--- xlsx2bmp.py
+++ xlsx2bmp.py
@@ -7,23 +7,25 @@ import win32com.client as win32 # 打开excel文件
 from ctypes import*
 
 os.system("getjson.py") #此脚本运行生成out.xlsx文件
-path = 'C:\\Users\\DELL\\Desktop\\theme\\theme\\'
-excel = win32.Dispatch('Excel.Application') #获取Excel
-excel.Visible = False    #进程可见，False是它暗自进行
-excel.DisplayAlerts = 0   #不跳出警告  
-wb = excel.Workbooks.Open(path+'out.xlsx',False) #打开文件，有时候会有警告框说由外部链接什么的（与里面公式有关），要点是则True，否则False
-ws = wb.Worksheets('Summary')        # 获取Sheet“Summary”
 
-ws.Range('A1:D24').CopyPicture()    # 复制A1:D24图片区域
-ws.Paste(ws.Range('K1'))    # 将图片移动到K1
-ws.Shapes('Picture 1').Copy()    # 复制移动的图片Picture 1
-img = ImageGrab.grabclipboard()  # 获取图片数据
-img.save(path+'Picture 1.png') # 图片另存为
-wb.Close(False)    #关闭该文件，并保存。不保存就是False
-excel.quit()
+def xlsx2bmp():
+    path = 'C:\\Users\\DELL\\Desktop\\theme\\theme\\'
+    excel = win32.Dispatch('Excel.Application') #获取Excel
+    excel.Visible = False    #进程可见，False是它暗自进行
+    excel.DisplayAlerts = 0   #不跳出警告  
+    wb = excel.Workbooks.Open(path+'out.xlsx',False) #打开文件，有时候会有警告框说由外部链接什么的（与里面公式有关），要点是则True，否则False
+    ws = wb.Worksheets('Summary')        # 获取Sheet“Summary”
 
-im = Image.open('C:/Users/DELL/Desktop/theme/theme/Picture 1.png')
-im.save('11.bmp')
+    ws.Range('A1:D24').CopyPicture()    # 复制A1:D24图片区域
+    ws.Paste(ws.Range('K1'))    # 将图片移动到K1
+    ws.Shapes('Picture 1').Copy()    # 复制移动的图片Picture 1
+    img = ImageGrab.grabclipboard()  # 获取图片数据
+    img.save(path+'Picture 1.png') # 图片另存为
+    wb.Close(False)    #关闭该文件，并保存。不保存就是False
+    excel.quit()
+
+    im = Image.open('C:/Users/DELL/Desktop/theme/theme/Picture 1.png')
+    im.save('12.bmp')
 
 def setImage(aString):
     win32clipboard.OpenClipboard()
@@ -43,5 +45,9 @@ def send_qq(to_who,msg):
 
 to_who='张三' #好友/群QQ窗口须为活动状态
 msg=windll.user32.LoadImageW(0, r"11.bmp", win32con.IMAGE_BITMAP, 0, 0, win32con.LR_LOADFROMFILE)
-if msg != 0: #由于图片编码问题，图片载入失败的话，msg为0
+
+if __name__ == "__main__":
+    xlsx2bmp()
     send_qq(to_who,msg)
+#if msg != 0: #由于图片编码问题，图片载入失败的话，msg为0
+#    send_qq(to_who,msg)
